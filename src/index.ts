36cfb0b40243d7a683f084535889f32243fd3dd5
snowtiger <snowtigersoft@126.com>
2023-12-21 23:09:50 +0800

    support puzzle-wallet

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export {FoxWalletAdapter} from "./wallet-adapter/foxwallet";
+export {PuzzleWalletAdapter} from "./wallet-adapter/puzzle-wallet";
 export {SoterWalletAdapter} from "./wallet-adapter/soter-wallet";
 export {LeoWalletAdapter} from "@demox-labs/aleo-wallet-adapter-leo";
