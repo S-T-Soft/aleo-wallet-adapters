ca7d5afcba075f6970b357e620f7a8c21b8051ca
snowtiger <snowtigersoft@126.com>
2024-02-22 00:39:35 +0800

    Add avail wallet

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,5 @@ export {FoxWalletAdapter} from "./wallet-adapter/foxwallet.js";
 export {PuzzleWalletAdapter} from "./wallet-adapter/puzzle-wallet.js";
 export {SoterWalletAdapter} from "./wallet-adapter/soter-wallet.js";
 export {LeoWalletAdapter} from "@demox-labs/aleo-wallet-adapter-leo";
+export {AvailWalletAdapter} from "./wallet-adapter/avail-wallet.js";
 export {configureConnection as configureConnectionForPuzzle} from "@puzzlehq/sdk";
